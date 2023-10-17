561147a9a5f0924f89036356e9f1acf7dac1aadc
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-10-17 01:50:01 -0700

    Ch26: Makes the composable clickable. Adds weighting

--- app/src/main/java/com/bignerdranch/android/codapizza/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/codapizza/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
             CodaPizzaTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+//                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     ToppingCell(
--- app/src/main/java/com/bignerdranch/android/codapizza/ui/ToppingCell.kt
+++ app/src/main/java/com/bignerdranch/android/codapizza/ui/ToppingCell.kt
@@ -1,6 +1,5 @@
 package com.bignerdranch.android.codapizza.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,8 +9,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bignerdranch.android.codapizza.model.Topping
 import com.bignerdranch.android.codapizza.model.ToppingPlacement
 
@@ -42,13 +43,22 @@ fun ToppingCell(
     placement: ToppingPlacement?,
     onClickTopping: () -> Unit
 ) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clickable { onClickTopping() }
+            .padding(vertical = 4.dp, horizontal = 16.dp)
+    ) {
         Checkbox(
             checked = placement != null,
             onCheckedChange = {}
         )
 
-        Column {
+        Column(
+            modifier = Modifier
+                .weight(1f, fill = true)
+                .padding(start = 4.dp)
+        ) {
             Text(
                 text = stringResource(topping.toppingName),
                 style = MaterialTheme.typography.bodyLarge
