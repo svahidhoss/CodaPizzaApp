40bd71e8e6a59cc1313909ed3ad1b079b0e17152
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-10-17 02:37:37 -0700

    Ch26: Defines a new screen PizzaBuilderScreen and adds content to it.

---
+++ app/src/main/java/com/bignerdranch/android/codapizza/ui/PizzaBuilderScreen.kt
@@ -0,0 +1,66 @@
+package com.bignerdranch.android.codapizza.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bignerdranch.android.codapizza.R
+import com.bignerdranch.android.codapizza.model.Topping
+import com.bignerdranch.android.codapizza.model.ToppingPlacement
+import java.util.Locale
+
+@Preview
+@Composable
+fun PizzaBuilderScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        ToppingsList(
+            modifier = modifier
+                .fillMaxWidth()
+                .weight(1f, fill = true)
+        )
+    }
+
+    OrderButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    )
+}
+
+@Composable
+private fun ToppingsList(
+    modifier: Modifier = Modifier
+) {
+    ToppingCell(
+        topping = Topping.Pepperoni,
+        placement =
+        ToppingPlacement.Left,
+        onClickTopping = {},
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun OrderButton(
+    modifier: Modifier = Modifier
+) {
+    Button(
+        modifier = modifier,
+        onClick = {
+            // TODO
+        }
+    ) {
+        Text(
+            text = stringResource(R.string.place_order_button)
+                .toUpperCase(Locale.CANADA)
+        )
+    }
+}
