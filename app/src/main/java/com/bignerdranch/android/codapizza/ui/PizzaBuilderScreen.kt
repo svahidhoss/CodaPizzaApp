e0fb186c4909fd083dfa2dba362347bc5a424dd8
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2024-12-11 14:31:56 -0500

    Ch27: now stores the pizza var in a mutable state so that compose know of the change immediately.

--- app/src/main/java/com/bignerdranch/android/codapizza/ui/PizzaBuilderScreen.kt
+++ app/src/main/java/com/bignerdranch/android/codapizza/ui/PizzaBuilderScreen.kt
@@ -1,6 +1,5 @@
 package com.bignerdranch.android.codapizza.ui
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -9,6 +8,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,17 +41,7 @@ fun PizzaBuilderScreen(
     }
 }
 
-private var pizza =
-    Pizza(
-        toppings = mapOf(
-            Topping.Pepperoni to ToppingPlacement.All,
-            Topping.Pineapple to ToppingPlacement.All
-        )
-    )
-    set(value) {
-        Log.d("PizzaBuilderScreen", "Reassigned pizza to $value")
-        field = value
-    }
+private var pizza by mutableStateOf(Pizza())
 
 @Composable
 private fun ToppingsList(
